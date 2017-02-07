f40c97ef9c6dc8841d5be6cb548e2527ff8a8c20
maxime <maxime.leblond76@gmail.com>
2017-02-07 23:55:33 +0100

    modif des sz getclass

--- src/com/maxime/leblond/Bd.java
+++ src/com/maxime/leblond/Bd.java
@@ -2,22 +2,25 @@ package com.maxime.leblond;
 
 import java.util.Date;
 
+
 /**
  * Created by duche on 07/02/2017.
  */
 public class Bd extends Livre {
 
 
+
     public Bd(String titre, String auteur, Date dateParussion, int prix, String genre, String dessinateur) {
         super(titre, auteur, dateParussion, prix, genre);
         super.setDessinateur(dessinateur) ;
-        
+
+
     }
 
     @Override
     public String toString() {
         super.toString();
-        return getClass().getName()+" {" +
+        return getClass().getSimpleName()+" {" +
                 "titre='" + titre + '\'' +
                 ", Auteur='" + Auteur + '\'' +
                 ", dateParussion='" + dateParussion + '\'' +

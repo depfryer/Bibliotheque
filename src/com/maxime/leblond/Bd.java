91a9f772a70054e6aefbb266b595a1eaacacb27c
maxime <maxime.leblond76@gmail.com>
2017-02-07 18:06:12 +0100

    mini MAJ
    2/3 modif

--- src/com/maxime/leblond/Bd.java
+++ src/com/maxime/leblond/Bd.java
@@ -23,4 +23,5 @@ public class Bd extends Livre {
                 "Dessinateur='" + Dessinateur + '\'' +
                 '}';
     }
+
 }
--- src/com/maxime/leblond/Livre.java
+++ src/com/maxime/leblond/Livre.java
@@ -3,16 +3,21 @@ package com.maxime.leblond;
 /**
  * Created by duche on 07/02/2017.
  */
-public class Livre {
+public class Livre{
     String titre;
     String Auteur;
     String dateParussion;
     int prix;
     String Genre;
 
+
+    public String getTitre() {
+        return titre;
+    }
+
     @Override
     public String toString() {
-        return "Livre{" +
+        return "Livre{  " +
                 "titre='" + titre + '\'' +
                 ", Auteur='" + Auteur + '\'' +
                 ", dateParussion='" + dateParussion + '\'' +
@@ -21,6 +26,9 @@ public class Livre {
                 '}';
     }
 
+
+
+
     public Livre(String titre, String auteur, String dateParussion, int prix, String genre) {
         this.titre = titre;
         Auteur = auteur;
@@ -28,4 +36,5 @@ public class Livre {
         this.prix = prix;
         Genre = genre;
     }
+
 }

ced33ce5d62698b21e42f4270e50a6aed7af9fe7
maxime <maxime.leblond76@gmail.com>
2017-02-07 16:03:20 +0100

    Creation du livre

---
+++ src/com/maxime/leblond/Livre.java
@@ -0,0 +1,31 @@
+package com.maxime.leblond;
+
+/**
+ * Created by duche on 07/02/2017.
+ */
+public class Livre {
+    String titre;
+    String Auteur;
+    String dateParussion;
+    int prix;
+    String Genre;
+
+    @Override
+    public String toString() {
+        return "Livre{" +
+                "titre='" + titre + '\'' +
+                ", Auteur='" + Auteur + '\'' +
+                ", dateParussion='" + dateParussion + '\'' +
+                ", prix=" + prix +
+                ", Genre='" + Genre + '\'' +
+                '}';
+    }
+
+    public Livre(String titre, String auteur, String dateParussion, int prix, String genre) {
+        this.titre = titre;
+        Auteur = auteur;
+        this.dateParussion = dateParussion;
+        this.prix = prix;
+        Genre = genre;
+    }
+}
